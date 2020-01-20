4c049659f761602bef279681356e52d4b6ad5146
Jacob Frericks <jacob.frericks@gmail.com>
2020-01-20 16:11:15 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/aca_sandbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aca_sandbox
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
